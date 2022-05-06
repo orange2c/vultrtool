51f6cd4acc0e8799e8f8fea4a4c5be6862eae9e1
orange2c <3335447573@qq.com>
2022-05-06 17:59:13 +0800

    定时保存

--- vultrTool/vpsmodel_locations.cpp
+++ vultrTool/vpsmodel_locations.cpp
@@ -1,10 +1,12 @@
 #include "vpsmodel_locations.h"
 
 
+
 //QStringList VPSMODEL_LOCATIONS::countryNAME = {
 //    "美国", "法国", "英国",
 //    "日本", "荷兰"
 //};
+
 //airport VPSMODEL_LOCATIONS::find_aircode( QString code)
 //{
 //    airport all_aircode[5];
@@ -15,52 +17,88 @@
 
 
 
-//QStringList *locat_transfer(QStringList *source)
+
+//VPSMODEL_LOCATIONS::VPSMODEL_LOCATIONS()
 //{
-//    QStringList locations_cn ={ "美国-纽约", "美国-芝加哥", "美国-德克萨斯", "美国-洛杉矶", "美国-亚特兰大",
+
+//}
+
+//struct airport all[]=
+//{
+//    {.aircode="ewr", .translate="纽约",   },
+//    {.aircode="ord", .translate="芝加哥" },
+//};
+
+aircode::aircode( QString code, QString translate, countryENUM thecountry)
+{
+    airport = code;
+    cn =translate;
+    country = thecountry;
+    count++;
+}
+
+
+country_familly::country_familly(QString name)
+{
+    setname(name);
+}
+
+
+//QStringList locations_cn ={ "美国-纽约", "美国-芝加哥", "美国-德克萨斯", "美国-洛杉矶", "美国-亚特兰大",
 //                                "荷兰-阿姆斯特丹", "英国-伦敦", "德国-法兰克福", "美国-加利福尼亚", "澳大利亚-悉尼",
 //                               "加拿大-多伦多", "法国-巴黎", "日本-东京", "波兰-华沙", "西班牙-马德里",
 //                               "韩国-仁川", "美国-迈阿密", "新加坡", "瑞典-斯德哥尔摩", "墨西哥",
 //                               "澳大利亚-墨尔本", "美国-西雅图"
 //                             };
 
-//    QStringList locations_en = { "ewr","ord","dfw","lax","atl",
+//QStringList locations_en = { "ewr","ord","dfw","lax","atl",
 //                                "ams","lhr","fra","sjc","syd",
 //                                "yto","cdg","nrt","waw","mad",
 //                                "icn","mia","sgp","sto","mex",
 //                                "mel", "sea"
 //                               };
+//countryENUM locations_country[] = { EAmerican, EAmerican, EAmerican, EAmerican, EAmerican
 
-//    QStringList *locations = new QStringList();
-//    int j=0;
-//    for( int i = 0; i<source->size(); i++ )
-//    {
+//                                };
+
+void VPSMODEL_LOCATIONS::transfer( QStringList *source )
+{
+
+    aircode all_air[]={
+        aircode( "pwr","s", EAmerican ),
+
+    };
+
+    all_air[0].country = EAmerican ;
 
 
-//        for( j=0; j<locations_en.size(); j++ )
-//        {
+        QStringList *locations = new QStringList();
+        int j=0;
+        for( int i = 0; i<source->size(); i++ )
+        {
+            for( j=0; j<locations_en.size(); j++ )
+            {
 
+                if( QString::compare( source->at(i), locations_en.at(j) ) == 0 )
+                {
+                    locations->append( locations_cn.at(j) ); //
+                    locations_cn.removeAt(j);
+                    locations_en.removeAt(j); //删除查找到的项，下次查找时就能减少时间
+                    break;
+                }
+                else if( j == locations_en.size()-1 )//即遍历到数组最后一个也没找到匹配的
+                {
+                    locations->append( source->at(i) ); //则原样返回
+                }
+            }
 
-//            if( QString::compare( source->at(i), locations_en.at(j) ) == 0 )
-//            {
-//                locations->append( locations_cn.at(j) ); //
-//                locations_cn.removeAt(j);
-//                locations_en.removeAt(j); //删除查找到的项，下次查找时就能减少时间
-//                break;
-//            }
-//            else if( j == locations_en.size()-1 )//即遍历到数组最后一个也没找到匹配的
-//            {
-//                locations->append( source->at(i) ); //则原样返回
-//            }
-//        }
+        }
 
-//    }
-//    return locations;
+}
+VPSMODEL_LOCATIONS::~VPSMODEL_LOCATIONS()
+{
+    for(int i=0; i<EcountryMAX; i++ )
+        delete allfamilly[i];
+}
 
-//}
-
-//VPSMODEL_LOCATIONS::VPSMODEL_LOCATIONS()
-//{
-
-//}
 
--- vultrTool/vpsmodel_locations.h
+++ vultrTool/vpsmodel_locations.h
@@ -1,29 +1,44 @@
 #ifndef VPSMODEL_LOCATIONS_H
 #define VPSMODEL_LOCATIONS_H
 #include <QStringList>
+#include "linkedlist.h"
 
+enum countryENUM
+{
+    EAmerican=0, EFrance, EEngland,
+    EJapan,  EHolland,
+    EcountryMAX
+}; //国家枚举
 
-//enum countryENUM
-//{
-//    Amreican=0, France, England,
-//    Japan,  Holland,
-//}; //国家枚举
-
-//struct airport
-//{
-//    QString aircode;  //机场3字代码
-//    countryENUM country;  //归属国家
-//    QString translate;  //对应机场的中文名称
-//};
-
+class aircode
+{
+public:
+    QString airport ;  //机场3字代码
+    QString cn ;  //对应机场的中文名称
+    countryENUM country;
+    aircode( QString code, QString translate, countryENUM thecountry);
+    ~aircode(){ count--; }
+    static int count; //存储到底被实例化多少对象
+};
+int aircode::count = 0;
+class country_familly:public LinkedList<aircode>
+{
+public:
+    country_familly(QString name);
+};
 
 class VPSMODEL_LOCATIONS
 {
 public:
-    VPSMODEL_LOCATIONS();
+    VPSMODEL_LOCATIONS( ); //传入一个机型可用的所有机场代码
+    ~VPSMODEL_LOCATIONS();
+
+    void transfer(QStringList *source);
+
+    country_familly *locations[EcountryMAX];
+    int size_locations;
 protected:
-//    static QStringList countryNAME;
-//    airport find_aircode( QString code);
+    country_familly *allfamilly[EcountryMAX];
 
 };
 
